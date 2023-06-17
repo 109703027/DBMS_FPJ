1236f3dcce47246165427959e6ef531234599e93
Chi-Hung Chang <chihung816224@gmail.com>
2023-06-18 01:48:53 +0800

    [style] coding style

--- member_course.py
+++ member_course.py
@@ -1,9 +1,9 @@
-import csv
 import sqlite3
-from flask import Flask, g, render_template, request, redirect, url_for, Blueprint
+from flask import g, render_template, request, redirect, url_for, Blueprint
 
 member_course_router = Blueprint("member_course_router", __name__)
 SQLITE_DB_PATH = 'gym.db'
+
 
 def get_db():
     db = getattr(g, '_database', None)
@@ -13,10 +13,16 @@ def get_db():
         db.execute("PRAGMA foreign_keys = ON")
     return db
 
+
 @member_course_router.route('/', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return redirect(url_for('member_course_router.all_course', username=request.form.get('username')))
+        return redirect(
+            url_for(
+                'member_course_router.all_course',
+                username=request.form.get('username')
+            )
+        )
 
     return render_template('mem_login.html')
 
@@ -31,54 +37,124 @@ def all_course(username):
     cursor.execute(money_sql, (username,))
     money_data = cursor.fetchone()
     user_data = {
-        'ID':username,
-        'money':money_data[0]
+        'ID': username,
+        'money': money_data[0]
     }
 
-    sql = """SELECT c.courseTitle, ch.name, c.courseDay, c.courseTime, c.dateStart, c.dateEnd, c.courseID, c.cost, ch.coachID
-             FROM course AS c, coach AS ch
-             WHERE c.dateStart > date(\'now\') and c.coachID = ch.coachID
-             ORDER BY c.dateStart"""
+    sql = """
+        SELECT
+            c.courseTitle,
+            ch.name,
+            c.courseDay,
+            c.courseTime,
+            c.dateStart,
+            c.dateEnd,
+            c.courseID,
+            c.cost,
+            ch.coachID
+        FROM
+            course AS c,
+            coach AS ch
+        WHERE
+            c.dateStart > date(\'now\') AND
+            c.coachID = ch.coachID
+        ORDER BY
+            c.dateStart
+    """
     data = db.execute(sql).fetchall()
     course_data = []
+
     for d in data:
-        
-        params1 = (d[6],username)
+        params1 = (d[6], username)
         params2 = (d[6],)
-        params3 = (username,d[5])
-        if db.execute("SELECT * FROM record WHERE courseID = ? and memberID = (SELECT memberID FROM member WHERE memberID = ?)",params1).fetchone():
+        params3 = (username, d[5])
+
+        if db.execute("""
+            SELECT
+                *
+            FROM
+                record
+            WHERE
+                courseID = ? AND
+                memberID = (
+                    SELECT
+                        memberID
+                    FROM
+                        member
+                    WHERE
+                        memberID = ?
+                )
+        """, params1).fetchone():
             cond = 1
-        elif db.execute("SELECT * FROM record WHERE courseID = ? GROUP BY courseID HAVING COUNT(*) >= 10",params2).fetchone():
+        elif db.execute("""
+            SELECT
+                *
+            FROM
+                record
+            WHERE
+                courseID = ?
+            GROUP BY
+                courseID HAVING COUNT(*) >= 10
+        """, params2).fetchone():
             cond = 2
-        elif db.execute("SELECT * FROM member WHERE memberID = ? and memberExp < ?",params3).fetchone():
+        elif db.execute("""
+            SELECT
+                *
+            FROM
+                member
+            WHERE
+                memberID = ? AND
+                memberExp < ?
+        """, params3).fetchone():
             cond = 3
         else:
             cond = 4
 
         course_data.append({
-            'Title':d[0],
-            'c_Name':d[1],
-            'Day':d[2]+"  "+d[3],
-            'Start':d[4],
-            'End':d[5],
-            'Condition':cond,
-            'courseID':d[6],
-            'Cost':d[7],
-            'coachID':d[8]
+            'Title': d[0],
+            'c_Name': d[1],
+            'Day': d[2] + "  " + d[3],
+            'Start': d[4],
+            'End': d[5],
+            'Condition': cond,
+            'courseID': d[6],
+            'Cost': d[7],
+            'coachID': d[8]
         })
+
     return render_template(
         'all_course.html',
-        user_data = user_data,
-        course_data = course_data
+        user_data=user_data,
+        course_data=course_data
     )
+
 
 @member_course_router.route('/my_course/<username>')
 def my_course(username):
     db = get_db()
-    sql = """SELECT C.courseTitle, T.name, C.courseDay, C.courseTime, C.dateStart, C.dateEnd, R.evaluate, C.courseID
-            FROM course AS C, coach AS T, member as M, record as R
-            WHERE C.coachID = T.coachID and R.courseID = C.courseID and R.memberID = M.memberID and M.memberID = ?
-            ORDER BY c.dateStart"""
+    sql = """
+        SELECT
+            C.courseTitle,
+            T.name,
+            C.courseDay,
+            C.courseTime,
+            C.dateStart,
+            C.dateEnd,
+            R.evaluate,
+            C.courseID
+        FROM
+            course AS C,
+            coach AS T,
+            member as M,
+            record as R
+        WHERE
+            C.coachID = T.coachID AND
+            R.courseID = C.courseID AND
+            R.memberID = M.memberID AND
+            M.memberID = ?
+        ORDER BY
+            C.dateStart
+    """
     cursor = db.cursor()
     cursor.execute(sql, (username,))
     data = cursor.fetchall()
@@ -86,21 +162,21 @@ def my_course(username):
 
     for d in data:
         course_data.append({
-            'Title':d[0],
-            'c_Name':d[1],
-            'Day':d[2]+"  "+d[3],
-            'Start':d[4],
-            'End':d[5],
-            'comment':d[6],
-            'courseID':d[7]
+            'Title': d[0],
+            'c_Name': d[1],
+            'Day': d[2] + "  " + d[3],
+            'Start': d[4],
+            'End': d[5],
+            'comment': d[6],
+            'courseID': d[7]
         })
-
 
     return render_template(
         'my_course.html',
-        userID = username,
-        course_data = course_data
+        userID=username,
+        course_data=course_data
     )
+
 
 @member_course_router.route('/save', methods=['POST'])
 def save_comment():
@@ -116,6 +192,7 @@ def save_comment():
     db.commit()
 
     return redirect(url_for('member_course_router.my_course', username=userID))
+
 
 @member_course_router.route('/insert', methods=['GET', 'POST'])
 def sign_up():
@@ -139,11 +216,14 @@ def sign_up():
 
     return redirect(url_for('member_course_router.my_course', username=userID))
 
-#@member_course_router.teardown_appcontext
+
+# @member_course_router.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
+
     if db is not None:
         db.close()
+
 
 if __name__ == '__main__':
     member_course_router.run(debug=True)
